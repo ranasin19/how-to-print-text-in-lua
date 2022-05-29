851e6ab6c7f44ad0f2030a7793965a92810dbfff
Rana omar <80989305+ranasin19@users.noreply.github.com>
2022-05-29 20:57:15 +0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+print ("hello world!!")
+local opening = display.newText("hello world",150,50,native.systemFont,24)
+opening:setFillColor(0,1,0)
